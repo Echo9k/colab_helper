5e73b7103f85973b05074b6321a8fb11ac7ef539
Guillermo Alcantara <guimoAAGG@gmail.com>
2020-07-09 12:11:43 -0600

    Reorganized files
    Reorganized without modification.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Colab_helper',
+      version='0.1.0-dev20200709',
+      description='Helper functions to download, unzip, and augment images for steganalysis',
+      packages=['Colab_helper'],
+      zip_safe=False)
