63663687eb672a2a161a3c45bdcf6c6d8d301feb
Guillermo Alcantara <guimoAAGG@gmail.com>
2020-07-09 12:19:23 -0600

    leading caps in setup package.
    removed leading cap from package name

--- colab_helper/__init__.py
+++ colab_helper/__init__.py
@@ -2,5 +2,5 @@ from .Colab_helper import Colab_helper
 from .GetData import GetData, _list_folders, _prevent_duplicates
 from .HardwareAssistant import Rig, info, gpu_info, tpu_info, default_strategy
 
-__version__ = '0.1.0-dev20200708'
+__version__ = '0.1.0-dev20200709'
 __author__ = 'Guillermo Alcántara Gonzälez (Echo9k)'
