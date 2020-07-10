26d4eb69c7e59ea8274296476d02a98d4071507a
Guillermo Alcantara <guimoAAGG@gmail.com>
2020-07-10 14:30:35 -0600

    mkdir
    automatically makes the img_dir

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='colab_helper',
-      version='0.1.0-dev20200709',
+      version='0.1.1-dev20200710',
       description='Helper functions to download, unzip, and augment images for steganalysis',
       packages=['colab_helper'],
       zip_safe=False)
