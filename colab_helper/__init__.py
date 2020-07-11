c9384b0776d4f42fdde4fc1e9f0c8eb98cbe0ee9
Guillermo Alcantara <guimoAAGG@gmail.com>
2020-07-10 22:40:10 -0600

    added argument
    added list_classes argument @GetData.GetData.img_batch()
    Commented out the Hardware Assistant sheet

--- colab_helper/__init__.py
+++ colab_helper/__init__.py
@@ -3,5 +3,5 @@ from .GetData import GetData, _list_folders, _prevent_duplicates
 from .HardwareAssistant import Rig, info, gpu_info, tpu_info, default_strategy
 from .Plots import plot_minibatch
 
-__version__ = '0.1.1-dev20200710'
+__version__ = '0.1.1-kag20200711'
 __author__ = 'Guillermo Alcántara Gonzälez (Echo9k)'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='colab_helper',
-      version='0.1.1-dev20200710',
+      version='0.1.1-kag20200711',
       description='Helper functions to download, unzip, and augment images for steganalysis',
       packages=['colab_helper'],
       zip_safe=False)
